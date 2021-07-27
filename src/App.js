c4df4f37b222a3ceaf12451e286569474e6f5ab5
Hongleang Lim <lim.hongleang@gmail.com>
2021-07-27 15:20:18 +1000

    setup SCSS variables

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Navbar from "./components/Navbar/Navbar";
-import Homepage from "./pages/Homepage/Homepage";
-// import TopGameList from './pages/TopGameList/TopGameList';
 
 const App = () => {
-  const APIKey = "a60e9faf7fmshc23154d0472737cp1baaf5jsna13ea55c29e8";
 
   return (
     <Router>
-      <Navbar />
       <Switch>
         <Route exact path="/"></Route>
       </Switch>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,58 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-
-import Button from '../Button/Button';
 import './Navbar.scss';
 
 const Navbar = () => {
-  const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
-
-  const handleClick = () => setClick(!click);
-
-  const closeMobileMenu = () => setClick(false);
-
-  const showButton = () => {
-    //Add responsiveness to button
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
-
-  window.addEventListener("resize", showButton);
-
   return ( 
-    <div>
-      <nav className="navbar">
-        <div className="navbar__container">
-          <Link to='/' className="navbar__logo">
-            F2Play
-          </Link>
-          <div className="menu-icon">
-            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
-          </div>
-          <ul className={click ? "navbar__menu active": "navbar__menu"}>
-            <li className="navbar__item">
-              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                Home
-              </Link>
-              <Link to="/TopGames" className="nav-links" onClick={closeMobileMenu}>
-                Top Games
-              </Link>
-              <Link to="/register-form" className="nav-links" onClick={closeMobileMenu}>
-                Sign in / Sign up
-              </Link>
-            </li>
-          </ul>
-          {/* Render Button if button is true */}
-          {button && <Button></Button>}
-        </div>
-      </nav>
-    </div>
-  );
+  
+   );
 }
  
 export default Navbar;
