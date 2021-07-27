1d90ea4b0ab6c991a35f94bf7403d2d515d5afd8
Hongleang Lim <lim.hongleang@gmail.com>
2021-07-27 16:43:27 +1000

    Navbar: completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Navbar from './components/Navbar/Navbar';
 
 
 const App = () => {
 
   return (
     <Router>
+      <Navbar></Navbar>
       <Switch>
         <Route exact path="/"></Route>
       </Switch>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,22 @@
 import './Navbar.scss';
 
+
 const Navbar = () => {
   return ( 
-  
+    <nav className="navbar">
+      <a className="navbar__logo" href="/">F2P <i class="fas fa-dice"></i> GAMES</a>
+      <ul className="navbar__menu">
+        <li className="navbar__item">
+          <a href="/" className="navbar__link">Home</a>
+        </li>
+        <li className="navbar__item">
+          <a href="/topgames" className="navbar__link">Top Games</a>
+        </li>
+        <li className="navbar__item">
+          <a href="/sign-up" className="navbar__link">Sign up</a>
+        </li>
+      </ul>
+    </nav>
    );
 }
  
