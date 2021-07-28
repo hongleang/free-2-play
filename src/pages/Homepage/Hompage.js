22ba4a090b3bc9cc9bd45fc79db0bf2100fec476
hongleang <lim.hongleang@gmail.com>
2021-07-28 10:22:34 +1000

    reinstall npm

--- src/pages/Homepage/Hompage.js
+++ src/pages/Homepage/Hompage.js
@@ -12,27 +12,7 @@ const Homepage = () => {
         <hr />
       </div>
       <div className="container-fluid">
-        <div id="carousel" className="homepage__slider carousel slide" data-interval="false">
-          <div class="homepage__slider__wrapper carousel-inner">
-             <div class="homepage__slider__item carousel-item active">
-              <img src="https://source.unsplash.com/random" class="" alt="..."></img>
-            </div>
-            <div class="homepage__slider__item carousel-item">
-              <img src="https://www.publicdomainpictures.net/pictures/320000/nahled/background-image.png" class="" alt="..."></img>
-            </div>
-            <div class="homepage__slider__item carousel-item">
-              <img src="https://source.unsplash.com/random" class="" alt="..."></img>
-            </div>
-          </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
-        </div>
+        
       </div>
     </div>
   );
